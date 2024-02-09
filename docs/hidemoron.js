e0c9230059e6008ec6d1b33eaca422e0c91a0933
ElindiOwl <158224188+ElindiOwl@users.noreply.github.com>
2024-02-09 20:05:49 +0300

    Delete docs directory

--- docs/hidemoron.js
+++
@@ -1,9 +0,0 @@
-function hidemoron() {
-    let x = document.getElementsByClassName("hidemoron");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-}
-function buttonClick(elem) {
-    elem.value = 'Nice one!';
-}
